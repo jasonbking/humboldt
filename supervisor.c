8224f066e8fc7b5f65ac3da7d5885d916fc17525
Alex Wilson <alex.wilson@joyent.com>
2017-09-14 01:15:18 +0000

    more work on keygen

--- supervisor.c
+++ supervisor.c
@@ -39,27 +39,163 @@
 
 struct token_slot *token_slots = NULL;
 
+static char *
+expand_key_and_replace(const char *input, size_t inlen, size_t outlen)
+{
+	char *out;
+	size_t pos, len;
+	char buf[crypto_hash_sha512_BYTES];
+
+	out = calloc(1, outlen);
+	assert(out != NULL);
+
+	explicit_bzero(&buf, sizeof (buf));
+	crypto_hash_sha512(buf, input, inlen);
+	do {
+		len = crypto_hash_sha512_BYTES;
+		if (len > outlen)
+			len = outlen;
+		bcopy(buf, out, len);
+		pos += len;
+		outlen -= len;
+		if (outlen > 0)
+			crypto_hash_sha512(buf, buf, sizeof (buf));
+	} while (outlen > 0);
+	explicit_bzero(&buf, sizeof (buf));
+	explicit_bzero(input, inlen);
+	free(input);
+	return (out);
+}
+
+static void
+encrypt_and_write_key(struct sshkey *key, struct yubikey *yk, const char *dir,
+    const char *fn)
+{
+	int rv, i;
+	char *chal, *key, *iv, *encdata, *packdata;
+	size_t challen, keylen, keysz, ivlen, authlen, blocksz, enclen, packlen;
+	struct sshcipher_ctx *cipher, *cctx;
+	nvlist_t *nv;
+	const char *ciphername = "chacha20-poly1305@openssh.com";
+	struct sshbuf *buf;
+	librename_atomic_t *rast;
+	FILE *f;
+
+	buf = sshbuf_new();
+	assert(buf != NULL);
+
+	challen = 64;
+	chal = calloc(1, challen);
+	assert(chal != NULL);
+	arc4random_buf(chal, challen);
+
+	cipher = cipher_by_name(ciphername);
+	assert(cipher != NULL);
+
+	authlen = cipher_authlen(cipher);
+	blocksz = cipher_blocksize(cipher);
+
+	ivlen = cipher_ivlen(cipher);
+	iv = calloc(1, ivlen);
+	assert(iv != NULL);
+	arc4random_buf(iv, ivlen);
+
+	keysz = cipher_keylen(cipher);
+	key = calloc(1, keysz);
+	assert(key != NULL);
+
+	ykc_txn_begin(yk);
+	rv = ykc_select(yk);
+	assert(rv == 0);
+	keylen = keysz;
+	rv = ykc_hmac(yk, 1, chal, challen, key, &keylen);
+	assert(rv == 0);
+	assert(keylen < keysz);
+	ykc_txn_end(yk);
+
+	if (keylen < keysz)
+		key = expand_key_and_replace(key, keylen, keysz);
+
+	rv = cipher_init(&cctx, cipher, key, keysz, iv, ivlen, 1);
+	assert(rv == 0);
+
+	rv = sshkey_private_serialize(key, buf);
+	assert(rv == 0);
+	i = 0;
+	while (sshbuf_len(buf) % blocksz) {
+		rv = sshbuf_put_u8(buf, ++i & 0xff);
+		asset(rv == 0);
+	}
+	enclen = sshbuf_len(buf) + authlen;
+	encdata = calloc(1, enclen);
+	rv = cipher_crypt(cctx, 0, encdata, sshbuf_ptr(buf), sshbuf_len(ptr),
+	    0, authlen);
+	assert(rv == 0);
+	sshbuf_reset(buf);
+	cipher_free(cctx);
+
+	assert(nvlist_alloc(&nv, NV_UNIQUE_NAME, 0) == 0);
+	assert(nvlist_add_uint8(nv, "version", 1) == 0);
+	assert(nvlist_add_string(nv, "algorithm", ciphername) == 0);
+	assert(nvlist_add_byte_array(nv, "encdata", encdata, enclen) == 0);
+	assert(nvlist_add_byte_array(nv, "challenge", chal, challen) == 0);
+	assert(nvlist_add_byte_array(nv, "iv", iv, ivlen) == 0);
+	assert(nvlist_add_uint32(nv, "yk_serial", yk->yk_serial) == 0);
+	assert(nvlist_add_uint8(nv, "yk_slot", 1) == 0);
+
+	packdata = NULL;
+	packlen = 0;
+	assert(nvlist_pack(nv, &packdata, &packlen, NV_ENCODE_XDR) == 0);
+	assert(packdata != NULL);
+	assert(packlen > 0);
+
+	rv = librename_atomic_init(dir, fn, NULL, 0600, 0, &rast);
+	assert(rv == 0);
+	f = fdopen(librename_atomic_fd(rast));
+	assert(fwrite(packdata, packlen, 1, f) == 1);
+	assert(fflush(f) == 0);
+	rv = librename_atomic_commit(rast);
+	assert(rv == 0);
+	librename_atomic_fini(rast);
+
+	explicit_bzero(key, keysz);
+	free(key);
+	explicit_bzero(chal, challen);
+	free(chal);
+	explicit_bzero(iv, ivlen);
+	free(iv);
+	explicit_bzero(encdata, enclen);
+	free(encdata);
+	sshbuf_free(buf);
+}
+
 static void
 generate_keys(const char *zonename, const char *keydir)
 {
 	struct sshkey *authkey;
 	struct sshkey *certkey;
-	int rv;
+	int rv, i;
 	SCARDCONTEXT ctx;
-	struct yubikey *yk;
+	char fn[PATH_MAX];
 
 	rv = SCardEstablishContext(SCARD_SCOPE_SYSTEM, NULL, NULL, &ctx);
 	assert(rv == SCARD_S_SUCCESS);
-	yk = ykc_find(ctx);
 
+	yk = ykc_find(ctx);
 	assert(yk != NULL);
 	assert(yk->yk_next == NULL);
 
 	rv = sshkey_generate(KEY_ED25519, 256, &authkey);
-	assert(rv != 0);
+	assert(rv == 0);
+	snprintf(fn, sizeof (fn), "%s/auth.key", keydir);
+	encrypt_and_write_key(authkey, yk, fn);
+	sshkey_free(authkey);
 
 	rv = sshkey_generate(KEY_RSA, 2048, &certkey);
-	assert(rv != 0);
+	assert(rv == 0);
+	snprintf(fn, sizeof (fn), "%s/cert.key", keydir);
+	encrypt_and_write_key(certkey, yk, fn);
+	sshkey_free(certkey);
 }
 
 static void
